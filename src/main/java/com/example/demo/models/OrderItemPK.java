4eff8ce33bb0e6c7b28574d5042303c0744f997e
Matheus Farias Fernandes <matheus_farias_fernandes@outlook.com>
2022-07-11 16:05:13 -0300

    OrderPk

--- src/main/java/com/example/demo/models/OrderItemPK.java
+++ src/main/java/com/example/demo/models/OrderItemPK.java
@@ -1,10 +1,49 @@
 package com.example.demo.models;
 
+import java.io.Serializable;
+
+import org.hibernate.annotations.ManyToAny;
+
 import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+@Embeddable
+public class OrderItemPK implements Serializable{
+    
+    @ManyToOne
+    @JoinColumn(name = "order_id")
+     private Order order;
 
-public class OrderItemPK {
+     @ManyToOne
+     @JoinColumn(name = "product_id")
+     private Product product;  
 
-     
+    /**
+     * @return Order return the order
+     */
+    public Order getOrder() {
+        return order;
+    }
 
+    /**
+     * @param order the order to set
+     */
+    public void setOrder(Order order) {
+        this.order = order;
+    }
+
+    /**
+     * @return Product return the product
+     */
+    public Product getProduct() {
+        return product;
+    }
+
+    /**
+     * @param product the product to set
+     */
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 
 }
